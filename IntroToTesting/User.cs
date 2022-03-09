c2d8854e08b79870ca63e6cd9fa1b256015e2ca8
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-03-09 10:22:42 -0700

    Initial Commit

---
+++ IntroToTesting/User.cs
@@ -0,0 +1,26 @@
+﻿namespace IntroToTesting
+{
+    public class User
+    {
+        public string Name { get; }
+        public string UserName { get; set; }
+        public List<string> Tweets { get; }
+
+        public User(string name)
+        {
+            Name = name;
+            Tweets = new List<string>();
+        }
+
+        public void Tweet(string message)
+        {
+            Tweets.Add(message);
+        }
+
+        public string MostRecentTweet()
+        {
+            var lastIndex = Tweets.Count - 1;
+            return Tweets[lastIndex];
+        }
+    }
+}
